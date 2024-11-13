e03e96d0e55a992bda6fd8a79da352553b5f8b7a
LaneyS05 <laney.staggs05@gmail.com>
2024-11-13 16:45:27 -0500

    sliding imgs

--- my_bakery/static/js/home.js
+++ my_bakery/static/js/home.js
@@ -7,3 +7,24 @@ function toggleDropdown() {
     dropdown.style.display = "block";
   }
 }
+
+const images = [
+  "../static/assets/bakery.jpg",
+  "../static/assets/bread.jpg", // Add your next image path here
+  "../static/assets/sweets.jpg", // Add another image path here
+  "../static/assets/pastries.jpg",
+];
+
+let currentImageIndex = 0;
+
+// Function to change the image
+function changeImage(direction) {
+  if (direction === "next") {
+    currentImageIndex = (currentImageIndex + 1) % images.length; // Go to next image
+  } else if (direction === "prev") {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length; // Go to previous image
+  }
+
+  // Update the image source
+  document.getElementById("bakeryImage").src = images[currentImageIndex];
+}
